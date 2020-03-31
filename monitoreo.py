11446df507bae200d9787a14e56a8bcc81847da9
s <s>
2020-03-31 15:28:27 -0500

    test_01

--- monitoreo.py
+++ monitoreo.py
@@ -1,7 +1,7 @@
 from app import app
 from flask import Flask, url_for
 
-app.run(debug=True, port=808)
+app.run(debug=True, port=8080)
 # app.run(host='0.0.0.0', debug=True, port=8080)
 # app.add_url_rule('/favicon.ico',redirect_to=url_for('static', filename='monitor.ico'))
 
