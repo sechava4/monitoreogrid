6d0d41073bf86ad4ce710a23da3e5cf438904c89
s <s>
2020-04-01 16:45:55 -0500

    add requeriments

--- app/__init__.py
+++ app/__init__.py
@@ -5,12 +5,10 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
 
-
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 Bootstrap(app)
-
 
 if db.engine.url.drivername == 'sqlite':
     migrate = Migrate(app, db)
