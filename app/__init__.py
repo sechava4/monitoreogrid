d21847e3acb3a9d0b27e3444ac252ed9f92c10e1
s <s>
2020-04-03 13:12:45 -0500

    digitalocean2

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
-app.config.from_object(devConfig)
+app.config.from_object(ProdConfig)
 #app.config.from_object(ProdConfig)
 print(devConfig.SQLALCHEMY_DATABASE_URI)
 db = SQLAlchemy(app)
