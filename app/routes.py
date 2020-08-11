3c57ee07795ae78343dea6aa16c971fc98d30328
sechava4 <sechava4@PTOF56559.cinfo.eafit.edu.co>
2020-08-11 16:13:11 -0500

    efficiency calculation

--- app/routes.py
+++ app/routes.py
@@ -314,7 +314,7 @@ def add_entry():
             Fw = m * 9.81 * math.sin(slope)
             operation.net_force = (m * float(request.args["mean_acc"])) + Fw + operation.friction_force
             operation.mec_power = (operation.net_force * float(request.args["mean_speed"])/3.6) * 1.3 / 1000 * 1.34
-            operation.eff = 100 * operation.mec_power / operation.power_kw
+            operation.eff = 100 * float(operation.mec_power) / float(operation.power_kw)
             operation.en_pot = rise * 9.81 * m
 
             db.session.add(operation)
