81ae57c59e11a5e1546ec37250d9437c2221b667
auribev1 <auribev1@eafit.edu.co>
2022-02-24 11:42:41 -0500

    Update open_dataframes.py

--- managev_app/open_dataframes.py
+++ managev_app/open_dataframes.py
@@ -115,7 +115,7 @@ def get_heights(vehicle, var, d1, h1, h2):
         df = df[["latitude", "longitude", "timestamp", var]]
         df["name"] = df[var]
         # Remove outliers
-        df.fillna(method='bfill',inplace=True)
+        df.fillna(0,inplace=True)
         df = df[(np.abs(stats.zscore(df[var])) < 3)]
 
         x_max = df[var].max()
