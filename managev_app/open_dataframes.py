7a9e951daf4cc5f9f6d8321dd07ca29ee2bae2cf
auribev1 <auribev1@eafit.edu.co>
2022-02-24 09:42:11 -0500

    Update open_dataframes.py

--- managev_app/open_dataframes.py
+++ managev_app/open_dataframes.py
@@ -111,7 +111,7 @@ def get_heights(vehicle, var, d1, h1, h2):
 
     if all(
         elem in df.columns for elem in ["latitude", "longitude", "timestamp", var]
-    ) and len(df[var]):
+    ) and len(df[var]) and vehicle.placa != "BICI01":
         df = df[["latitude", "longitude", "timestamp", var]]
         df["name"] = df[var]
         # Remove outliers
